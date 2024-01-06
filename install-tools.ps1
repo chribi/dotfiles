d4e2fffa647ec767ee554f3e9d374a749689a8bb
Christoph Bier <christoph.bier@gmx.de>
2024-01-06 14:00:52 +0100

    Improve ps experience

--- install-tools.ps1
+++ install-tools.ps1
@@ -6,3 +6,8 @@ winget install BurntSushi.ripgrep.MSVC
 winget install dandavison.delta
 winget install JanDeDobbeleer.OhMyPosh
 winget install neovim.neovim
+
+Install-Module PSReadLine
+Install-Module Terminal-Icons
+Install-Module PSFzf
+Install-Module Posh-Git
--- psprofile.ps1
+++ psprofile.ps1
@@ -4,7 +4,17 @@
 # . "$DOTFILES/psprofile.ps1"
 # ```
 oh-my-posh init pwsh --config "$DOTFILES/ohmyposh.json" | Invoke-Expression
+$env:POSH_GIT_ENABLED = $true
+
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
+
+Import-Module PSReadLine
+Import-Module Posh-Git
+Import-Module PSFzf
+Import-Module Terminal-Icons
+
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
+Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
 
 $env:RIPGREP_CONFIG_PATH="$DOTFILES/ripgreprc"
 $env:BAT_CONFIG_PATH="$DOTFILES/batrc"
@@ -30,10 +40,24 @@ Set-Alias -Name '....' -Value Move-UpUpUp
 
 Set-Alias -Name 'll' -Value Get-ChildItem
 
-Function Get-GitStatus { git status }
 Set-Alias -Name 'gti' -Value git
-Set-Alias -Name 'gs' -Value Get-GitStatus
 
+Function Alias-Gs { git status }
+Set-Alias -Name 'gs' -Value Alias-Gs
+Function Alias-Add { git add }
+Set-Alias -Name add -Value Alias-Add
+Function Alias-Gd { git diff }
+Set-Alias -Name gd -Value Alias-Gd
+Function Alias-Gds { git diff --staged }
+Set-Alias -Name gds -Value Alias-Gds
+Function Alias-Gco { git commit }
+Set-Alias -Name gco -Value Alias-Gco
+Function Alias-Gca { git commit -a }
+Set-Alias -Name gca -Value Alias-Gca
+Function Alias-Amend { git commit --amend }
+Set-Alias -Name amend -Value Alias-Amend
+Function Alias-Amendwq { git commit --amend --no-edit }
+Set-Alias -Name amendwq -Value Alias-Amendwq
 
 ## }}}
 
